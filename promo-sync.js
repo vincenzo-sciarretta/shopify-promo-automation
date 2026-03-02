7cd63b57b7f601ae98dd678014b04de5b3aeb4d2
vincenzo-sciarretta <sciarretta@sgunion.it>
2026-03-02 12:37:15 +0100

    Update promo-sync.js

--- promo-sync.js
+++ promo-sync.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const SHOP = process.env.SHOPIFY_STORE;
 const ACCESS_TOKEN = process.env.SHOPIFY_TOKEN;
 
-const API_VERSION = '2024-01';
+const API_VERSION = '2024-10';
 
 const GRAPHQL_ENDPOINT = `https://${SHOP}/admin/api/${API_VERSION}/graphql.json`;
 
